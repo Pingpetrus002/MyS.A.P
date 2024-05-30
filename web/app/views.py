9410dcd7926c9041e72b383e5b3b6173272edf75
Sularec Pierre <86714770+Pingpetrus002@users.noreply.github.com>
2024-05-30 16:22:08 +0200

    aa

--- web/app/views.py
+++ web/app/views.py
@@ -2,8 +2,6 @@ from app import app
 from flask import render_template
 
 
-REQUEST_COUNT = Counter('flask_app_request_count', 'Total HTTP requests')
-REQUEST_LATENCY = Gauge('flask_app_request_latency_seconds', 'Latency of HTTP requests')
 
 
 @app.route('/')
