81c5d43fe08810c5e0f8a700a09111e5e87ee9d9
Sularec Pierre <86714770+Pingpetrus002@users.noreply.github.com>
2024-05-30 16:09:03 +0200

    aa

--- web/app/views.py
+++ web/app/views.py
@@ -1,6 +1,5 @@
 from app import app
 from flask import render_template
-from prometheus_client import generate_latest, CollectorRegistry, Counter, Gauge, Response
 
 
 REQUEST_COUNT = Counter('flask_app_request_count', 'Total HTTP requests')
@@ -10,10 +9,4 @@ REQUEST_LATENCY = Gauge('flask_app_request_latency_seconds', 'Latency of HTTP re
 @app.route('/')
 def home():
     return render_template('index.html')
-
-
-@app.route('/metrics')
-def metrics():
-
-    return Response(generate_latest(), mimetype='text/plain')
 
