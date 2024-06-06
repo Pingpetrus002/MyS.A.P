4caa30e2e7a9a37659e09705d970cb75e61ba839
RobinCouture <147134048+RobinCouture@users.noreply.github.com>
2024-06-06 09:57:15 +0200

    feat(init.sql):update db struct

--- mysql/init.sql
+++ mysql/init.sql
@@ -4,219 +4,177 @@ CREATE DATABASE IF NOT EXISTS flask_app;
 
 USE flask_app;
 
-CREATE TABLE IF NOT EXISTS alternant(
-   Id INT,
-   Nom VARCHAR(50),
-   Prenom VARCHAR(50),
-   mail VARCHAR(50),
-   promotion VARCHAR(50),
-   PRIMARY KEY(Id)
-);
-
 CREATE TABLE IF NOT EXISTS suiveur(
-   Id INT,
+   Id_suiveur INTEGER NOT NULL AUTO_INCREMENT,
    Nom VARCHAR(50),
-   Prenom VARCHAR(50),
+   prenom VARCHAR(50),
    date_naissance DATE,
    statut VARCHAR(50),
    mail VARCHAR(50),
-   PRIMARY KEY(Id)
+   PRIMARY KEY(Id_suiveur)
 );
 
 CREATE TABLE IF NOT EXISTS planning(
-   id INT,
-   PRIMARY KEY(id)
-);
-
-CREATE TABLE IF NOT EXISTS mission(
-   id INT,
-   PRIMARY KEY(id)
-);
-
-CREATE TABLE IF NOT EXISTS evaluation(
-   id INT,
-   PRIMARY KEY(id)
-);
-
-CREATE TABLE IF NOT EXISTS document(
-   id INT,
-   nom VARCHAR(50),
-   md5 VARCHAR(50),
-   type VARCHAR(50),
-   PRIMARY KEY(id)
-);
-
-CREATE TABLE IF NOT EXISTS entreprise(
-   id INT,
-   raison_sociale VARCHAR(200),
-   adresse VARCHAR(200),
-   PRIMARY KEY(id)
+   id_planning INTEGER NOT NULL AUTO_INCREMENT,
+   diplome VARCHAR(50),
+   annee VARCHAR(50),
+   classe VARCHAR(50),
+   PRIMARY KEY(id_planning)
 );
 
 CREATE TABLE IF NOT EXISTS ecole(
-   id INT,
+   id_ecole INTEGER NOT NULL AUTO_INCREMENT,
    raison_sociale VARCHAR(200),
    adresse VARCHAR(200),
-   PRIMARY KEY(id)
+   PRIMARY KEY(id_ecole)
 );
 
 CREATE TABLE IF NOT EXISTS rp(
-   Id INT,
+   Id_rp INTEGER NOT NULL AUTO_INCREMENT,
    Nom VARCHAR(50),
-   Prenom VARCHAR(50),
+   prenom VARCHAR(50),
    date_naissance DATE,
    mail VARCHAR(50),
    statut VARCHAR(50),
-   PRIMARY KEY(Id)
+   PRIMARY KEY(Id_rp)
 );
 
 CREATE TABLE IF NOT EXISTS rre(
-   Id INT,
+   Id_rre INTEGER NOT NULL AUTO_INCREMENT,
    Nom VARCHAR(50),
-   Prenom VARCHAR(50),
+   prenom VARCHAR(50),
    date_naissance DATE,
    mail VARCHAR(50),
-   PRIMARY KEY(Id)
+   PRIMARY KEY(Id_rre)
 );
 
 CREATE TABLE IF NOT EXISTS programme(
-   id INT,
-   PRIMARY KEY(id)
-);
-
-CREATE TABLE IF NOT EXISTS contrat(
-   id INT,
-   PRIMARY KEY(id)
-);
-
-CREATE TABLE IF NOT EXISTS conflit(
-   id INT,
-   PRIMARY KEY(id)
+   id_programme INTEGER NOT NULL AUTO_INCREMENT,
+   diplome VARCHAR(50),
+   annee VARCHAR(50),
+   Id_rp INT NOT NULL,
+   PRIMARY KEY(id_programme),
+   FOREIGN KEY(Id_rp) REFERENCES rp(Id_rp)
 );
 
 CREATE TABLE IF NOT EXISTS cre(
-   Id INT,
+   Id_cre INTEGER NOT NULL AUTO_INCREMENT,
    Nom VARCHAR(50),
-   Prenom VARCHAR(50),
+   prenom VARCHAR(50),
    date_naissance DATE,
    mail VARCHAR(50),
-   Id_1 INT,
-   Id_2 INT,
-   Id_3 INT,
-   Id_4 INT,
-   id_5 INT,
-   PRIMARY KEY(Id),
-    UNIQUE(Id_1),
-   UNIQUE(Id_2),
-   UNIQUE(Id_3),
-   UNIQUE(Id_4),
-     FOREIGN KEY(Id_1) REFERENCES rre(Id),
-   FOREIGN KEY(Id_2) REFERENCES rre(Id),
-   FOREIGN KEY(Id_3) REFERENCES rp(Id),
-   FOREIGN KEY(Id_4) REFERENCES suiveur(Id),
-   FOREIGN KEY(id_5) REFERENCES ecole(id)
+   Id_rre INT NOT NULL,
+   Id_rre_1 INT,
+   Id_rp INT,
+   Id_suiveur INT,
+   id_ecole INT,
+   PRIMARY KEY(Id_cre),
+   UNIQUE(Id_rre_1),
+   UNIQUE(Id_rp),
+   UNIQUE(Id_suiveur),
+   FOREIGN KEY(Id_rre) REFERENCES rre(Id_rre),
+   FOREIGN KEY(Id_rre_1) REFERENCES rre(Id_rre),
+   FOREIGN KEY(Id_rp) REFERENCES rp(Id_rp),
+   FOREIGN KEY(Id_suiveur) REFERENCES suiveur(Id_suiveur),
+   FOREIGN KEY(id_ecole) REFERENCES ecole(id_ecole)
+);
+
+CREATE TABLE IF NOT EXISTS entreprise(
+   id_entreprise INTEGER NOT NULL AUTO_INCREMENT,
+   raison_sociale VARCHAR(200),
+   adresse VARCHAR(200),
+   id_ecole INT NOT NULL,
+   PRIMARY KEY(id_entreprise),
+   FOREIGN KEY(id_ecole) REFERENCES ecole(id_ecole)
+);
+
+CREATE TABLE IF NOT EXISTS contrat(
+   id_contrat INTEGER NOT NULL AUTO_INCREMENT,
+   libelle VARCHAR(50),
+   datecreation VARCHAR(50),
+   datesuppression VARCHAR(50),
+   Id_cre INT NOT NULL,
+   PRIMARY KEY(id_contrat),
+   FOREIGN KEY(Id_cre) REFERENCES cre(Id_cre)
+);
+
+CREATE TABLE IF NOT EXISTS conflit(
+   id_conflit INTEGER NOT NULL AUTO_INCREMENT,
+   libelle VARCHAR(50),
+   datecreation DATE,
+   datesuppression DATE,
+   Id_cre INT NOT NULL,
+   PRIMARY KEY(id_conflit),
+   FOREIGN KEY(Id_cre) REFERENCES cre(Id_cre)
 );
 
 CREATE TABLE IF NOT EXISTS tuteur_entreprise(
-   Id INT,
+   Id_tuteur INTEGER NOT NULL AUTO_INCREMENT,
    Nom VARCHAR(50),
-   Prenom VARCHAR(50),
+   prenom VARCHAR(50),
    date_naissance DATE,
    mail VARCHAR(50),
    entreprise VARCHAR(50),
-   Id_1 INT NOT NULL,
-   id_2 INT NOT NULL,
-   PRIMARY KEY(Id),
-   UNIQUE(Id_1),
-   FOREIGN KEY(Id_1) REFERENCES alternant(Id),
-   FOREIGN KEY(id_2) REFERENCES entreprise(id)
+   id_entreprise INT NOT NULL,
+   id_ecole INT NOT NULL,
+   Id_suiveur INT NOT NULL,
+   Id_rp INT NOT NULL,
+   PRIMARY KEY(Id_tuteur),
+   FOREIGN KEY(id_entreprise) REFERENCES entreprise(id_entreprise),
+   FOREIGN KEY(id_ecole) REFERENCES ecole(id_ecole),
+   FOREIGN KEY(Id_suiveur) REFERENCES suiveur(Id_suiveur),
+   FOREIGN KEY(Id_rp) REFERENCES rp(Id_rp)
 );
 
-CREATE TABLE IF NOT EXISTS collabore(
-   id INT,
-   id_1 INT,
-   PRIMARY KEY(id, id_1),
-   FOREIGN KEY(id) REFERENCES entreprise(id),
-   FOREIGN KEY(id_1) REFERENCES ecole(id)
+CREATE TABLE IF NOT EXISTS alternant(
+   Id_alternant INTEGER NOT NULL AUTO_INCREMENT,
+   Nom VARCHAR(50),
+   prenom VARCHAR(50),
+   date_naissance DATE,
+   mail VARCHAR(50),
+   promotion VARCHAR(50),
+   id_planning INT NOT NULL,
+   Id_tuteur INT NOT NULL,
+   id_entreprise INT,
+   PRIMARY KEY(Id_alternant),
+   FOREIGN KEY(id_planning) REFERENCES planning(id_planning),
+   FOREIGN KEY(Id_tuteur) REFERENCES tuteur_entreprise(Id_tuteur),
+   FOREIGN KEY(id_entreprise) REFERENCES entreprise(id_entreprise)
 );
 
-CREATE TABLE IF NOT EXISTS affecte(
-   Id INT,
-   id_1 INT,
-   id_2 INT,
-   id_3 INT,
-   PRIMARY KEY(Id, id_1, id_2, id_3),
-   FOREIGN KEY(Id) REFERENCES alternant(Id),
-   FOREIGN KEY(id_1) REFERENCES planning(id),
-   FOREIGN KEY(id_2) REFERENCES mission(id),
-   FOREIGN KEY(id_3) REFERENCES evaluation(id)
+CREATE TABLE IF NOT EXISTS mission(
+   id_mission INTEGER NOT NULL AUTO_INCREMENT,
+   libelle VARCHAR(50),
+   description VARCHAR(50),
+   datedebut DATE,
+   datefin DATE,
+   Id_alternant INT,
+   PRIMARY KEY(id_mission),
+   FOREIGN KEY(Id_alternant) REFERENCES alternant(Id_alternant)
 );
 
-CREATE TABLE IF NOT EXISTS accede(
-   Id INT,
-   id_1 INT,
-   PRIMARY KEY(Id, id_1),
-   FOREIGN KEY(Id) REFERENCES alternant(Id),
-   FOREIGN KEY(id_1) REFERENCES document(id)
+CREATE TABLE IF NOT EXISTS evaluation(
+   id_evaluation INTEGER NOT NULL AUTO_INCREMENT,
+   dateevaluation DATE,
+   Id_alternant INT NOT NULL,
+   PRIMARY KEY(id_evaluation),
+   FOREIGN KEY(Id_alternant) REFERENCES alternant(Id_alternant)
 );
 
-CREATE TABLE IF NOT EXISTS Communique(
-   Id INT,
-   Id_1 INT,
-   Id_2 INT,
-   PRIMARY KEY(Id, Id_1, Id_2),
-   FOREIGN KEY(Id) REFERENCES suiveur(Id),
-   FOREIGN KEY(Id_1) REFERENCES tuteur_entreprise(Id),
-   FOREIGN KEY(Id_2) REFERENCES rp(Id)
+CREATE TABLE IF NOT EXISTS document(
+   id_document INTEGER NOT NULL AUTO_INCREMENT,
+   nom VARCHAR(50),
+   md5 VARCHAR(50),
+   type VARCHAR(50),
+   datecreation DATE,
+   datesuppression DATE,
+   Id_suiveur INT NOT NULL,
+   Id_alternant INT NOT NULL,
+   PRIMARY KEY(id_document),
+   FOREIGN KEY(Id_suiveur) REFERENCES suiveur(Id_suiveur),
+   FOREIGN KEY(Id_alternant) REFERENCES alternant(Id_alternant)
 );
 
-CREATE TABLE IF NOT EXISTS redige(
-   Id INT,
-   id_1 INT,
-   PRIMARY KEY(Id, id_1),
-   FOREIGN KEY(Id) REFERENCES suiveur(Id),
-   FOREIGN KEY(id_1) REFERENCES document(id)
-);
-
-CREATE TABLE IF NOT EXISTS evalue(
-   Id INT,
-   Id_1 INT,
-   PRIMARY KEY(Id, Id_1),
-   FOREIGN KEY(Id) REFERENCES alternant(Id),
-   FOREIGN KEY(Id_1) REFERENCES tuteur_entreprise(Id)
-);
-
-CREATE TABLE IF NOT EXISTS informe(
-   Id INT,
-   id_1 INT,
-   PRIMARY KEY(Id, id_1),
-   FOREIGN KEY(Id) REFERENCES tuteur_entreprise(Id),
-   FOREIGN KEY(id_1) REFERENCES ecole(id)
-);
-
-CREATE TABLE IF NOT EXISTS supervice(
-   Id INT,
-   id_1 INT,
-   PRIMARY KEY(Id, id_1),
-   FOREIGN KEY(Id) REFERENCES rp(Id),
-   FOREIGN KEY(id_1) REFERENCES programme(id)
-);
-
-CREATE TABLE IF NOT EXISTS gere(
-   id INT,
-   Id_1 INT,
-   PRIMARY KEY(id, Id_1),
-   FOREIGN KEY(id) REFERENCES contrat(id),
-   FOREIGN KEY(Id_1) REFERENCES cre(Id)
-);
-
-CREATE TABLE IF NOT EXISTS resoud(
-   id INT,
-   Id_1 INT,
-   PRIMARY KEY(id, Id_1),
-   FOREIGN KEY(id) REFERENCES conflit(id),
-   FOREIGN KEY(Id_1) REFERENCES cre(Id)
-);
 
 CREATE USER root@root IDENTIFIED BY 'root';
 GRANT ALL PRIVILEGES ON flask_app.* TO root@root;
