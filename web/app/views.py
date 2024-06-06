ca2609cc9b5b94b27358716a4cf15767c16db94d
SuperBananeMan <damien.lolz.destructor@gmail.com>
2024-06-06 10:43:19 +0200

    Ajout react initial

--- web/app/views.py
+++ web/app/views.py
@@ -1,9 +1,11 @@
 from app import app
-from flask import render_template
+from flask import render_template, jsonify
 
 
 
-
+@app.route('/api/data')
+def get_data():
+    return jsonify({"message": "Hello from Flask!"})
 @app.route('/')
 def home():
     return render_template('index.html')
