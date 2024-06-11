7c878b00961522a6aea296ddd4823ecbf11b615b
Pingpetrus002 <86714770+Pingpetrus002@users.noreply.github.com>
2024-06-11 22:39:38 +0200

    fix: add 1h expire token

--- backend/app/config.py
+++ backend/app/config.py
@@ -9,4 +9,5 @@ class Config:
     DEBUG = True
     TESTING = False
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
+    JWT_ACCESS_TOKEN_EXPIRES = 3600
     SQLALCHEMY_DATABASE_URI= os.getenv('SQLALCHEMY_DATABASE_URI')
