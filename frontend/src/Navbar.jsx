e07ebb206594081ad76b273dbd00734120d3d69f
SuperBananeMan <damien.lolz.destructor@gmail.com>
2024-06-12 23:58:38 +0200

    Changements Navbar

--- frontend/src/Navbar.jsx
+++ frontend/src/Navbar.jsx
@@ -1,43 +1,22 @@
 import React, {useEffect, useState} from 'react';
 import logo from './assets/logo.svg';
-import Popover from '@mui/material/Popover';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
+import {Badge, Box} from "@mui/material";
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import AdbIcon from '@mui/icons-material/Adb';
+import NotificationsIcon from '@mui/icons-material/Notifications';
 
-function SimplePopover() {
-    const [anchorEl, setAnchorEl] = React.useState<HTMLButtonElement | null>null;
-
-    const handleClick = (event) => {
-        setAnchorEl(event.currentTarget);
-    };
-
-    const handleClose = () => {
-        setAnchorEl(null);
-    };
-
-    const open = Boolean(anchorEl);
-    const id = open ? 'simple-popover' : undefined;
-
-    return (
-        <div>
-            <Button aria-describedby={id} variant="contained" onClick={handleClick}>
-                Open Popover
-            </Button>
-            <Popover
-                id={id}
-                open={open}
-                anchorEl={anchorEl}
-                onClose={handleClose}
-                anchorOrigin={{
-                    vertical: 'bottom',
-                    horizontal: 'left',
-                }}
-            >
-                <Typography sx={{ p: 2 }}>The content of the Popover.</Typography>
-            </Popover>
-        </div>
-    );
-}
+const pages = ['Rapports', 'Étudiants', 'Rendez-vous'];
+const settings = ['Paramètre', 'Profil', 'Déconnexion'];
 
 const Navbar = () => {
     const [data, setData] = useState(null);
@@ -49,76 +28,162 @@ const Navbar = () => {
             .catch(error => console.error('Error fetching data:', error));
     }, []);
 
+    const [anchorElNav, setAnchorElNav] = React.useState(null);
+    const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+    const handleOpenNavMenu = (event) => {
+        setAnchorElNav(event.currentTarget);
+    };
+    const handleOpenUserMenuProfil = (event) => {
+        setAnchorElUser(event.currentTarget);
+    };
+
+    const handleOpenUserMenuNotif = (event) => {
+        setAnchorElUser(event.currentTarget);
+    };
+
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+    };
+
+    const handleCloseUserMenu = () => {
+        setAnchorElUser(null);
+    };
+
     return (
-        <nav>
-            <div style={{width: '100%', height: 151, background: '#FDD47C', border: '5px black solid'}}>
-                <img style={{width: 168.05, height: 151.25, borderRadius: 210.07}}
-                     src={logo} alt={'logo'}/>
-                <div style={{
-                    width: 1002.41,
-                    height: 46.21,
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    gap: 163,
-                    display: 'inline-flex'
-                }}>
-                    <div style={{
-                        width: 197.46,
-                        height: 46.21,
-                        color: 'black',
-                        fontSize: 37.81,
-                        fontFamily: 'Inter',
-                        fontWeight: '400',
-                        wordWrap: 'break-word'
-                    }}>Rapports
-                    </div>
-                    <div style={{
-                        width: 197.46,
-                        height: 46.21,
-                        color: 'black',
-                        fontSize: 37.81,
-                        fontFamily: 'Inter',
-                        fontWeight: '400',
-                        wordWrap: 'break-word'
-                    }}>Étudiants
-                    </div>
-                    <div style={{
-                        width: 281.49,
-                        height: 37.81,
-                        color: 'black',
-                        fontSize: 37.81,
-                        fontFamily: 'Inter',
-                        fontWeight: '400',
-                        wordWrap: 'break-word'
-                    }}>Rendez-vous
-                    </div>
-                </div>
-                <div style={{
-                    width: 204,
-                    height: 84.03,
-                    justifyContent: 'flex-end',
-                    alignItems: 'center',
-                    gap: 44,
-                    display: 'inline-flex'
-                }}>
-                    <i className="fa-solid fa-bell" style={{width: 75.62, height: 75.62, borderRadius: 210.07}}></i>
-                    <div style={{width: 29.41, height: 29.41, background: '#FF0000', borderRadius: 9999}}/>
-                    <div style={{
-                        width: 12.60,
-                        height: 21.01,
-                        color: 'white',
-                        fontSize: 21.01,
-                        fontFamily: 'Inter',
-                        fontWeight: '400',
-                        wordWrap: 'break-word'
-                    }}>2
-                    </div>
-                    <i className="fa-solid fa-user"
-                       style={{width: 84.03, height: 84.03, borderRadius: 210.07, border: "2px solid black"}}></i>
-                </div>
-            </div>
-            <SimplePopover/>
-        </nav>
+        <AppBar position="static">
+            <Container maxWidth="false" style={{height: 150, background: '#FDD47C', border: '5px black solid'}}>
+                <Toolbar disableGutters>
+                    <Typography
+                        variant="h6"
+                        noWrap
+                        component="a"
+                        href="/"
+                        sx={{
+                            mr: 2,
+                            display: {xs: 'none', md: 'flex'},
+                            fontFamily: 'monospace',
+                            fontWeight: 700,
+                            letterSpacing: '.3rem',
+                            color: 'inherit',
+                            textDecoration: 'none',
+                        }}
+                    >
+                        <img style={{width: '4.5rem', borderRadius: 210.07}}
+                             src={logo} alt={'logo'}/>
+                    </Typography>
+
+                    <Box sx={{flexGrow: 1, display: {xs: 'flex', md: 'none'}}}>
+                        <IconButton
+                            size="large"
+                            aria-label="account of current user"
+                            aria-controls="menu-appbar"
+                            aria-haspopup="true"
+                            onClick={handleOpenNavMenu}
+                            color="inherit"
+                        >
+                            <MenuIcon/>
+                        </IconButton>
+                        <Menu
+                            id="menu-appbar"
+                            anchorEl={anchorElNav}
+                            anchorOrigin={{
+                                vertical: 'bottom',
+                                horizontal: 'left',
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'left',
+                            }}
+                            open={Boolean(anchorElNav)}
+                            onClose={handleCloseNavMenu}
+                            sx={{
+                                display: {xs: 'block', md: 'none'},
+                            }}
+                        >
+                            {pages.map((page) => (
+                                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                                    <Typography textAlign="center">{page}</Typography>
+                                </MenuItem>
+                            ))}
+                        </Menu>
+                    </Box>
+                    <AdbIcon sx={{display: {xs: 'flex', md: 'none'}, mr: 1}}/>
+                    <Typography
+                        variant="h5"
+                        noWrap
+                        component="a"
+                        href="#app-bar-with-responsive-menu"
+                        sx={{
+                            mr: 2,
+                            display: {xs: 'flex', md: 'none'},
+                            flexGrow: 1,
+                            fontFamily: 'monospace',
+                            fontWeight: 700,
+                            letterSpacing: '.3rem',
+                            color: 'inherit',
+                            textDecoration: 'none',
+                        }}
+                    >
+                        LOGO
+                    </Typography>
+                    <Box sx={{flexGrow: 1, display: {xs: 'none', md: 'flex'}}}>
+                        {pages.map((page) => (
+                            <Button
+                                key={page}
+                                onClick={handleCloseNavMenu}
+                                sx={{my: 2, color: 'white', display: 'block'}}
+                            >
+                                {page}
+                            </Button>
+                        ))}
+                    </Box>
+
+                    <Box sx={{flexGrow: 0, p: 0, mr: 5 }} onClick={handleOpenUserMenuNotif}>
+                        <IconButton
+                            size="large"
+                            aria-label="show 17 new notifications"
+                            color="inherit"
+                        >
+                            <Badge badgeContent={17} color="error">
+                                <NotificationsIcon/>
+                            </Badge>
+                        </IconButton>
+                    </Box>
+
+                    <Box sx={{flexGrow: 0}}>
+                        <Tooltip title="Open settings">
+                            <IconButton onClick={handleOpenUserMenuProfil} sx={{p: 0}}>
+                                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg"/>
+                            </IconButton>
+                        </Tooltip>
+                        <Menu
+                            sx={{mt: '45px'}}
+                            id="menu-appbar"
+                            anchorEl={anchorElUser}
+                            anchorOrigin={{
+                                vertical: 'top',
+                                horizontal: 'right',
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'right',
+                            }}
+                            open={Boolean(anchorElUser)}
+                            onClose={handleCloseUserMenu}
+                        >
+                            {settings.map((setting) => (
+                                <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                                    <Typography textAlign="center">{setting}</Typography>
+                                </MenuItem>
+                            ))}
+                        </Menu>
+                    </Box>
+                </Toolbar>
+            </Container>
+        </AppBar>
     );
 };
 
