7c61ff548536fce82f44fe4b87245b0ba534204b
RobinCouture <rcouture1@myges.fr>
2024-06-17 19:05:05 +0200

    feat(Login.jsx):added style to login page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,13 +1,17 @@
 import { TextField, Button } from '@mui/material';
 import JObject from '../utils/JObject';
 import FetchWraper from '../utils/FetchWraper';
-
+import Grid from '@mui/material/Grid';
+import { alpha, styled } from '@mui/material/styles';
+import Logo from '../assets/logoFull.svg';
+import studentImage from '../assets/students.jpg?url';
+//<a href="https://fr.freepik.com/photos-gratuite/camarades-universite-etudient-ensemble_16294776.htm#query=travail%20etudiant&position=19&from_view=keyword&track=ais_user&uuid=b8fc33f8-2fb6-4c9a-bd49-fbc287ed69fb">Image de freepik</a> 
 
 async function SubmitLogin() {
     const form = document.getElementById('login-form');
-    
+
     let jObject = new JObject();
-    jObject.fromFormData( new FormData(form) );
+    jObject.fromFormData(new FormData(form));
     //console.log(jObject.toJSON());
 
     let fetchWraper = new FetchWraper();
@@ -49,29 +53,74 @@ async function SubmitLogout() {
 
 function Login() {
     return (
-        <div>
-            <h1>Login</h1>
-            <form id='login-form' style={{ display: 'flex', flexDirection: 'column' }}>
-                <TextField
-                    id="username"
-                    label="Username"
-                    variant="outlined"
-                    name='username'
-                />
-                <br />
-                <TextField
-                    id="password"
-                    label="Password"
-                    type="password"
-                    variant="outlined"
-                    name='password'
-                />
-                <br />
-                <Button type="button" onClick={SubmitLogin}>Submit</Button>
-                <Button type="button" onClick={SubmitLogout}>Logout</Button>
-            </form>
-        </div>
+        <body style={{ backgroundImage: `url("${studentImage}")`, backgroundSize: 'cover', width: '100vw', height: '100vh', margin: '0px', padding: '0px' }}>
+            <div style={{ color: 'white', backgroundColor: '#FDD47C', width: '400px', borderRadius: '45px', outline: '3px solid black', position: 'fixed', left: '50%', top: '50%', transform: 'translate(-50%, -50%)'}}>
+                <img src={Logo} alt="Logo" style={{ width: '100px', height: '100px', margin: '30px' }} />
+                <form id='login-form' style={{ display: 'flex', flexDirection: 'column', borderRadius: "90" }}>
+                    <Grid container spacing={2}>
+                        <Grid item xs={12}>
+                            <CustomTextField
+                                id="username"
+                                label="Identifiant"
+                                variant="outlined"
+                                name='username'
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <CustomTextField
+                                id="password"
+                                label="Mot de passe"
+                                type="password"
+                                variant="outlined"
+                                name='password'
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <CustomButton variant="outlined" color="inherit" onClick={SubmitLogin}>Connexion</CustomButton>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <Button type="button" onClick={SubmitLogout}>Logout</Button>
+                        </Grid>
+                    </Grid>
+                </form>
+            </div>
+        </body>
     );
 }
+
+const CustomTextField = styled(TextField)(({ theme }) => ({
+    "& .MuiOutlinedInput-root": {
+        color: "#000000",
+        backgroundColor: "#F2F4F8",
+        "& .MuiOutlinedInput-notchedOutline": {
+        },
+        "&.Mui-focused": {
+            "& .MuiOutlinedInput-notchedOutline": {
+                borderColor: "#000000",
+            },
+        },
+    },
+    "& .MuiInputLabel-outlined": {
+        "&.Mui-focused": {
+            color: "#000000",
+        },
+    },
+}))
+
+const CustomButton = styled(Button)(({ theme }) => ({
+    backgroundColor: '#ffffff',
+    color: '#000000',
+    borderWidth: '2px',
+    marginBottom: '20px',
+    "&.MuiButton-root": {
+        "&:hover": {
+            borderColor: '#000000',
+            backgroundColor: alpha('#ffffff', 0.8),
+            borderWidth: '2px',
+        },
+    },
+}));
 
 export default Login;
