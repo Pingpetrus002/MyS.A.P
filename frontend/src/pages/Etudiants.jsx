128511ae63c5e358e3e63282253ad397eb720d62
Maxoubiloux <max.kovacevic@laposte.net>
2024-06-20 14:14:47 +0200

    fix(Etudiant.jsx): delete title and button

--- frontend/src/pages/Etudiants.jsx
+++ frontend/src/pages/Etudiants.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import { Button, LinearProgress, Grid } from '@mui/material';
+import { useEffect, useState } from 'react';
+import { LinearProgress } from '@mui/material';
 
 // Importations personnalisées
 import FetchWraper from '../utils/FetchWraper';
@@ -61,41 +61,15 @@ export default function Etudiants() {
 
   // Rendu du composant
   return (
-    <Grid container justifyContent="center">
-      <Grid item xs={10} sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', marginTop: '3rem' }}>
-        <h1>Tous les Étudiants</h1>
-        <Button 
-          variant="contained" 
-          sx={{ 
-            backgroundColor: '#FDD47C', 
-            color: 'black', 
-            size: 'large',
-            borderRadius: '4px',
-            border: '2.5px solid #000000',
-            width: '40px', 
-            minWidth: '40px', 
-            height: '40px', 
-            fontSize: '24px', // Augmenter la taille du texte
-            '&:hover': {
-              backgroundColor: '#FFC039'
-            } 
-          }} 
-        >
-          +
-        </Button>
-      </Grid>
-      <Grid item xs={10}>
-        {loading ? (
-          <LinearProgress />
-        ) : (
-          <DataTable rows={students} type="etudiant" onRowButtonClick={handleRowClick} getRowId={getRowId} />
-        )}
-      </Grid>
-      <Grid item xs={10}>
-        {selectedStudent && (
-          <StudentModal student={selectedStudent} open={modalOpen} onClose={handleCloseModal} />
-        )}
-      </Grid>
-    </Grid>
+    <>
+      {loading ? (
+        <LinearProgress />
+      ) : (
+        <DataTable rows={students} type="etudiant" onRowButtonClick={handleRowClick} getRowId={getRowId} />
+      )}
+      {selectedStudent && (
+        <StudentModal student={selectedStudent} open={modalOpen} onClose={handleCloseModal} />
+      )}
+    </>
   );
 }
